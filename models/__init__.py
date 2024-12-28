aa4cc121354e89c5d1de0ddd64b4d501663db6cd
Andrii <andrey.n.bor@gmail.com>
2024-12-28 18:57:37 +0200

    Щось зробив  2.5

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,5 @@
-from . import hr_hospital_doctor, hr_hospital_patient, hr_hospital_diseases, hr_hospital_visits
-
+id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink
+access_hr_hospital_doctor_all,Access to Doctor (All),model_hr_hospital_doctor,,1,1,1,1
+access_hr_hospital_patient_all,Access to Patient (All),model_hr_hospital_patient,,1,1,1,1
+access_hr_hospital_diseases_all,Access to Diseases (All),model_hr_hospital_diseases,,1,1,1,1
+access_hr_hospital_visits_all,Access to Visits (All),model_hr_hospital_visits,,1,1,1,1
