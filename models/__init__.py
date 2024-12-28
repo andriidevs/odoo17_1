cfc942c30f8b5e6a509df2c16aec543232e2a1cf
Andrii <andrey.n.bor@gmail.com>
2024-12-28 23:52:34 +0200

    2.6 Виправив  init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
-id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink
-access_hr_hospital_doctor_all,Access to Doctor (All),model_hr_hospital_doctor,,1,1,1,1
-access_hr_hospital_patient_all,Access to Patient (All),model_hr_hospital_patient,,1,1,1,1
-access_hr_hospital_diseases_all,Access to Diseases (All),model_hr_hospital_diseases,,1,1,1,1
-access_hr_hospital_visits_all,Access to Visits (All),model_hr_hospital_visits,,1,1,1,1
+
+from . import hr_hospital_diseases
+from . import hr_hospital_doctor
+from . import hr_hospital_patient
+from . import hr_hospital_visits
